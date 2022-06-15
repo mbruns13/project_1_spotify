df77af503f4b21e0ff79e3ca6848d7484a471e02
Molly B <103689113+mbruns13@users.noreply.github.com>
2022-06-15 09:30:20 -0500

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-client_id = "85667fa3c0b84090825f14f31ef63684"
-client_secret = "20c9c94616cd4db397b1439f740210bc"
-
-access_token = 'BQDUIPiEEbhFWYhahAw2BbXhMta5amDw5U-gBpvC_HmxzPCFpxBh0kvW6vVGNvDG_2txclEibvaH8tYnHmY'
